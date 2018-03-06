161d9373046f27082540ea82b08e5abef08d2bd6
LehaNoisy <shumilovy@mail.ru>
2018-03-06 16:39:50 +0300

    remove Script4

--- script4.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-input=raw_input().split()
-N,M=map(int,input)
-a=[('.|.'*i).center(M,'-') for i in range(1,N,2)]
-for e in a+['WELCOME'.center(M,'-')]+list(reversed(a)):print(e)
-
-
-
