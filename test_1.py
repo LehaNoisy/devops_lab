775a2f33f66b6d52f33673e8806e6d8ad6547933
LehaNoisy <shumilovy@mail.ru>
2018-03-12 19:08:21 +0300

    Homework6

---
+++ test_1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import unittest
+from task6 import *
+
+from unittest import TestCase 
+
+class TestPrime(TestCase):
+
+
+  def test_is_prime(self):
+    """Test for is _prime"""
+    c1=Myclass()
+    self.assertFalse(c1.write_json())
+   
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
