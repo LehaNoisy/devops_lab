618435c495bbf80ab6cd20b0ea7a6aa94147be3f
LehaNoisy <shumilovy@mail.ru>
2018-03-13 12:00:54 +0300

    task5_changed

--- task5.py
+++ task5.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python
+class Solve(object):
+    def Circle(self, go):
+        """left right"""
+        lr = 0
+        """down up"""
+        dup = 0
+        for i in range(len(go)):
+            if(go[i] == 'R'):
+                lr = lr + 1
+            if (go[i] == 'L'):
+                lr = lr - 1
+            if(go[i] == 'U'):
+                dup = dup + 1
+            if(go[i] == 'D'):
+                dup = dup - 1
+        if(lr == 0 and dup == 0):
+            return True
+        else:
+            return False
 
-class Solve(object):  
-    def Circle(self, go):  
-        lr=0 #left_right  
-        dup=0 #down_up 
-        for i in range(len(go)):  
-            if(go[i]=='R'):  lr= lr + 1  
-            if (go[i] == 'L'):  lr = lr - 1  
-            if(go[i]=='U'):  dup=dup + 1  
-            if(go[i]=='D'):  dup=dup - 1  
-        if(lr==0 and dup==0):return True  
-        else: return False  
-  
-s=Solve()  
+s = Solve()
 
 print("Output:"),
-print(s.Circle("UD")) 
+print(s.Circle("UD"))
 
