4ceac3f8e58eb0534dd261de05be588575d669dd
LehaNoisy <shumilovy@mail.ru>
2018-03-13 14:24:57 +0300

    Task4_changed

--- task4.py
+++ task4.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
-
-input=raw_input().split()
-N,M=map(int,input)
-a=[('.|.'*i).center(M,'-') for i in range(1,N,2)]
-for e in a+['WELCOME'.center(M,'-')]+list(reversed(a)):print(e)
-
-
+input = raw_input().split()
+N, M = map(int, input)
+a = [('.|.'*i).center(M, '-') for i in range(1, N, 2)]
+for e in a+['WELCOME'.center(M, '-')]+list(reversed(a)):
+    print(e)
 
