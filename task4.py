8df13294d7a69f1a05132a3fbc5dd0a35c905dbe
Alexey <shumilovy@mail.ru>
2018-03-15 01:47:02 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+
+
 input = raw_input().split()
 N, M = map(int, input)
 a = [('.|.'*i).center(M, '-') for i in range(1, N, 2)]
 for e in a+['WELCOME'.center(M, '-')]+list(reversed(a)):
     print(e)
-
