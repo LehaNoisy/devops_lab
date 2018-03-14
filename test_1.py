f3ef63b2fce033c8aa7fccbf48deb50d68a8a631
Alexey <shumilovy@mail.ru>
2018-03-15 00:20:54 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
-import unittest
-from task6 import *
 
+import unittest
+from task6 import Myclass
 from unittest import TestCase
 
 
 class TestPrime(TestCase):
 
     def test_is_prime(self):
-        """Test for is _prime"""
+        """Test for clearis _prime"""
         c1 = Myclass()
         self.assertFalse(c1.write_json())
+
 
 if __name__ == '__main__':
     unittest.main()
